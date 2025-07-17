0247929aba34e3dbccd017670408cfd41111011b
putongongji <li-ten@foxmail.com>
2025-07-17 13:21:16 +0800

    Fix router configuration conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Layout } from 'antd'
 import Sidebar from './components/Sidebar'
 import ModelManagement from './pages/ModelManagement'
